a2bfc908d163b4effeee4c8ea843b6554d7609a3
Ronie083 <ahmedronie6@gmail.com>
2023-06-11 22:50:12 +0600

    delete funcionality added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dlzg3av.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,6 +56,14 @@ async function run() {
       const result = await enrolledCart.insertOne(course);
       res.send(result)
     })
+
+    app.delete('/enrolledCart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await enrolledCart.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
