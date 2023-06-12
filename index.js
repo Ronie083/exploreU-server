434fadf48bb5bcffc1afb5b44234c0866c6797d6
Ronie083 <ahmedronie6@gmail.com>
2023-06-12 13:54:11 +0600

    jwt token related work done

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoder) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoder = decoder;
+    next();
+  })
+}
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -33,14 +50,14 @@ async function run() {
     const enrolledCart = client.db("exploreUData").collection("coursesCart");
 
     app.post('/jwt', (req, res) => {
-      const user =req.body;
-      const jwtToken = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET, {
+      const user = req.body;
+      const jwtToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '1h'
       })
-      res.send({jwtToken})
+      res.send({ jwtToken })
     })
 
-    app.get('/users', async (req, res) =>{
+    app.get('/users', async (req, res) => {
       const result = await allUsers.find().toArray();
       res.send(result);
     })
@@ -56,9 +73,9 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           role: 'admin'
@@ -79,7 +96,7 @@ async function run() {
       const result = await allUsers.updateOne(query, updateDoc);
       res.send(result);
     });
-    
+
 
     app.get('/classes', async (req, res) => {
       const result = await allClasses.find().toArray();
@@ -91,11 +108,18 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/enrolledCart', async (req, res) => {
+    app.get('/enrolledCart', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
+
+      const decodedEmail = req.decoder.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+
+
       const query = { email: email };
       const result = await enrolledCart.find(query).toArray();
       res.send(result);
